9b40f421f23454b9f16f67d4cddc772772aff12b
swxft <edenweb@outlook.com>
2023-07-30 19:59:42 -0400

    add pagination

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ module.exports = (app) => {
 
   /* GET home page. */
   app.get('/', (req, res) => {
+    const page = req.query.page || 1
     Pet.paginate({}, {page: page}).then((results) => {
-      res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });
+      res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: results.page});
     });
   });
 }
